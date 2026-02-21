5c8d06477102820f0d734d4cb30790ebb17269ad
Bstz-att1 <hfdarioalexander7@gmail.com>
2026-02-21 18:50:22 -0500

    Fix host

--- servidor/script.js
+++ servidor/script.js
@@ -7,6 +7,10 @@ import {
     taskPatch
 } from './modules/index.js';
 
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Servidor escuchando en puerto 3000");
+});
+
 const totalForm = document.getElementById('task-form');
 const taskTitle = document.getElementById('titulo');
 const taskDescription = document.getElementById('descripcion');
